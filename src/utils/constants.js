d62d54101967be4f6236d604dfbb9359c5026f63
Arsenii Petrovich <arseniy.altoros@gmail.com>
2019-10-22 12:17:41 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -26,7 +26,7 @@ constants.minBallotDurationInDays = 2
 constants.startTimeOffsetInMinutes = 5
 constants.endTimeDefaultInMinutes = 2890
 constants.getTransactionReceiptInterval = 5000
-constants.rootPath = ''
+constants.rootPath = '/poa-dapps-voting'
 
 constants.navigationData = [
   {
