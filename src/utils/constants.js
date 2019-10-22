1415187b29f62b61e38e1c7121faa64effb88d1c
Arsenii Petrovich <arseniy.altoros@gmail.com>
2019-10-23 02:08:25 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -51,9 +51,37 @@ constants.navigationData = [
   }
 ]
 
+constants.SOKOL = 'sokol'
+constants.CORE = 'core'
+constants.DAI = 'dai'
+constants.KOVAN = 'kovan'
 constants.PCHAIN = 'pchain'
 
 constants.NETWORKS = {
+  '42': {
+    NAME: 'Kovan',
+    RPC: 'https://kovan.infura.io/v3/1125fe73d87c4e5396678f4e3089b3dd',
+    BRANCH: constants.KOVAN,
+    TESTNET: true
+  },
+  '77': {
+    NAME: 'Sokol',
+    RPC: 'https://sokol.poa.network',
+    BRANCH: constants.SOKOL,
+    TESTNET: true
+  },
+  '99': {
+    NAME: 'Core',
+    RPC: 'https://core.poa.network',
+    BRANCH: constants.CORE,
+    TESTNET: false
+  },
+  '100': {
+    NAME: 'Dai',
+    RPC: 'https://dai.poa.network',
+    BRANCH: constants.DAI,
+    TESTNET: false
+  },
   '191': {
     NAME: 'Pchain',
     RPC: 'http://bootnode-balancer-820434300.us-east-1.elb.amazonaws.com',
